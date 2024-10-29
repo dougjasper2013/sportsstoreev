94345d759723b5dcdfa56d57b4279461e4743df0
Douglas Jasper <doug.jasper@trios.com>
2024-10-29 15:46:26 -0400

    Initial Routing Complete

--- SportsStoreNS/src/app/store/store.component.ts
+++ SportsStoreNS/src/app/store/store.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { Product } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
 import { Cart } from "../model/cart.model";
+import { Router } from "@angular/router";
 
 @Component({
  selector: "store",
@@ -14,7 +15,9 @@ export class StoreComponent {
 
 
     constructor(private repository: ProductRepository,
-        private cart: Cart) { }
+        private cart: Cart,
+        private router: Router) { }
+
  get products(): Product[] {
     let pageIndex = (this.selectedPage - 1) * this.productsPerPage
  return this.repository.getProducts(this.selectedCategory)
@@ -53,6 +56,7 @@ get pageCount(): number {
 
 addProductToCart(product: Product) {
             this.cart.addLine(product);
+            this.router.navigateByUrl("/cart");
         }
 
 }
